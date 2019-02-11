53b9a5db4aac66720ea6afe4734e471651b2f817
btframework <44606899+btframework@users.noreply.github.com>
2019-02-12 01:19:43 +0300

    Add files via upload

---
+++ Joystick/Joystick.ino
@@ -0,0 +1,91 @@
+/*
+ * USB HID Joystick
+ * 
+ * Copyright (c) 2019 Mike Petrichenko
+ * http://www.tlmviewer.com
+ * https://www.btframework.com
+ * 
+ * The code based on NicoHood HID library.
+ * https://github.com/NicoHood/HID
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+*/
+
+#include "common.h"
+#include "usb.h"
+
+RxJoystick Joy;
+
+// Serial port between IO MCU and USB MCU
+#define hwSerial  Serial1
+//#define usbSerial Serial
+
+void setup()
+{
+  // Sends a clean report to the host. This is important on any Arduino type.
+  Joy.begin();
+
+  // Start USB Serial
+  //usbSerial.begin(0);
+
+  // Init hardware serial port.
+  hwSerial.end();
+  hwSerial.begin(57600);
+}
+
+uint16_t read()
+{
+  while (hwSerial.available() < 2) ;
+  return (hwSerial.read() << 8) | hwSerial.read();
+}
+
+void loop()
+{
+  // Wait for sync byte.
+  uint16_t sync = read;
+  while (sync != SYNC)
+  {
+    while (!hwSerial.available()) ;
+    sync = (sync << 8) | hwSerial.read();
+  }
+
+  uint16_t channels[MAX_CHANNELS];
+  for (uint8_t c = 0; c < MAX_CHANNELS; c++)
+    channels[c] = read();
+  
+  /*usbSerial.print("CH0: "); usbSerial.print(channels[0]); usbSerial.print(" ");
+  usbSerial.print("CH1: "); usbSerial.print(channels[1]); usbSerial.print(" ");
+  usbSerial.print("CH2: "); usbSerial.print(channels[2]); usbSerial.print(" ");
+  usbSerial.print("CH3: "); usbSerial.print(channels[3]); usbSerial.print(" ");
+  usbSerial.print("CH4: "); usbSerial.print(channels[4]); usbSerial.print(" ");
+  usbSerial.print("CH5: "); usbSerial.print(channels[5]); usbSerial.print(" ");
+  usbSerial.print("CH6: "); usbSerial.print(channels[6]); usbSerial.print(" ");
+  usbSerial.print("CH7: "); usbSerial.println(channels[7]);*/
+  
+  Joy.Throttle(channels[0]);
+  Joy.Aileron(channels[1]);
+  Joy.Elevator(channels[2]);
+  Joy.Rudder(channels[3]);
+  Joy.Gear(channels[4]);
+  Joy.Aux1(channels[5]);
+  Joy.Aux2(channels[6]);
+  Joy.Aux3(channels[7]);
+  
+  Joy.write();
+}
---
+++ Joystick/usb.h
@@ -0,0 +1,159 @@
+/*
+ * USB HID Joystick
+ * 
+ * Copyright (c) 2019 Mike Petrichenko
+ * http://www.tlmviewer.com
+ * https://www.btframework.com
+ * 
+ * The code based on NicoHood HID library.
+ * https://github.com/NicoHood/HID
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+*/
+
+#pragma once
+
+#include <Arduino.h>
+#include <HID.h>
+#include <HID-Settings.h>
+
+// For multiple reports support.
+#define HID_REPORTID_RX_JOYSTICK  11
+
+// USB Joystick HID device descriptor.
+static const uint8_t HID_DESCRIPTOR_JOYSTICK[] PROGMEM =
+{
+  /* Joystick with 8 axis */
+  0x05, 0x01,                     /* USAGE_PAGE (Generic Desktop) */
+  0x09, 0x04,                     /* USAGE (Joystick) */
+  0xa1, 0x01,                     /* COLLECTION (Application) */
+  0x85, HID_REPORTID_RX_JOYSTICK, /*   REPORT_ID */
+  /* 8 16bit Axis */
+  0x05, 0x01,                     /*   USAGE_PAGE (Generic Desktop) */
+  0x09, 0x01,                     /*   USAGE (Pointer) */
+  0xa1, 0x00,                     /*   COLLECTION (Physical) */
+  0x09, 0x36,                     /*     USAGE (Slider) for Throttle */
+  0x09, 0x30,                     /*     USAGE (X) for Roll (Aileron) */
+  0x09, 0x31,                     /*     USAGE (Y) for Pitch (Elevator)  */
+  0x09, 0x32,                     /*     USAGE (Z) for Rudder */
+  0x09, 0x33,                     /*     USAGE (Rx) for Gear */
+  0x09, 0x34,                     /*     USAGE (Ry) for Aux 1 */
+  0x09, 0x35,                     /*     USAGE (Rz) for Aux 2 */
+  0x09, 0x37,                     /*     USAGE (Dial) for Aux 3 */
+  0x16, 0xEB, 0x03,               /*     LOGICAL_MINIMUM (1000) */
+  0x26, 0xD0, 0x07,               /*     LOGICAL_MAXIMUM (2000) */
+  0x75, 0x10,                     /*     REPORT_SIZE (16) */
+  0x95, 0x08,                     /*     REPORT_COUNT (8) */
+  0x81, 0x02,                     /*     INPUT (Data,Var,Abs) */
+  0xc0,                           /*   END_COLLECTION */
+  0xc0                            /* END_COLLECTION */
+};
+
+// Internal data storage type.
+typedef struct
+{
+  int16_t Throttle;
+  int16_t Aileron;
+  int16_t Elevator;
+  int16_t Rudder;
+  int16_t Gear;
+  int16_t Aux1;
+  int16_t Aux2;
+  int16_t Aux3;
+} HID_RX_JOYSTICK_REPORT;
+  
+class RxJoystick
+{
+private:
+  HID_RX_JOYSTICK_REPORT FReport;
+
+  inline void SendReport(void* data, int length)
+  {
+    HID().SendReport(HID_REPORTID_RX_JOYSTICK, data, length);
+  }
+  
+public:
+  RxJoystick()
+  {
+    static HIDSubDescriptor node(HID_DESCRIPTOR_JOYSTICK, sizeof(HID_DESCRIPTOR_JOYSTICK));
+    HID().AppendDescriptor(&node);
+  }
+  
+  inline void begin()
+  {
+    end();
+  }
+  
+  void end()
+  {
+    memset(&FReport, 0x00, sizeof(FReport));
+    SendReport(&FReport, sizeof(FReport));
+  }
+  
+  inline void write()
+  {
+    SendReport(&FReport, sizeof(FReport)); 
+  }
+  
+  inline void reset()
+  {
+    memset(&FReport, 0x00, sizeof(FReport)); 
+  }
+
+  inline void Throttle(int16_t a)
+  {
+    FReport.Throttle = a;
+  }
+  
+  inline void Aileron(int16_t a)
+  {
+    FReport.Aileron = a; 
+  }
+  
+  inline void Elevator(int16_t a)
+  {
+    FReport.Elevator = a; 
+  }
+  
+  inline void Rudder(int16_t a)
+  {
+    FReport.Rudder = a; 
+  }
+  
+  inline void Gear(int16_t a)
+  {
+    FReport.Gear = a; 
+  }
+  
+  inline void Aux1(int16_t a)
+  {
+    FReport.Aux1 = a; 
+  }
+  
+  inline void Aux2(int16_t a)
+  {
+    FReport.Aux2 = a; 
+  }
+  
+  inline void Aux3(int16_t a)
+  {
+    FReport.Aux3 = a;
+  }
+};
+
